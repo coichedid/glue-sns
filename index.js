786c94c48c4434686d3a49b3b1c3fb3411d7ea21
Clovis Chedid <coichedid@gmail.com>
2019-01-30 14:57:47 -0200

    New client methods abstractions: topic creation and subscription listing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const AWS = require("aws-sdk");
 const Transform = require('stream').Transform;
+const async = require('async');
 
 class SNSError {
   constructor(message, code) {
@@ -33,6 +34,113 @@ class SNSClient {
           return reject(err);
         }
         return resolve();
+      });
+    });
+  }
+
+  getTopicArn(topicName) {
+    return `arn:aws:sns:us-east-1:117232195543:${topicName}`;
+  }
+
+  getTopicAttributes(topicName) {
+    return new Promise( (resolve, reject) => {
+      const topicArn = this.getTopicArn(topicName);
+      const params = {
+        TopicArn: topicArn
+      }
+
+      this.sns.getTopicAttributes(params, (err, data) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+
+        return resolve(data.Attributes);
+      } );
+    });
+  }
+
+  createIfNotExists(topicName, displayName, policy, ) { //Idempotent function only creates a topic if not exists
+    return new Promise( (resolve, reject) => {
+      const params = {
+        Name: topicName,
+        Attributes: {
+          DisplayName: displayName,
+          Policy: policy
+        }
+      }
+
+      this.sns.createTopic(params, (err, data) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+        return resolve(data);
+      })
+    });
+  }
+
+  subscribeToTopic(topicName, protocol, endpoint, filterPolicy) {
+    return new Promise( (resolve, reject) => {
+      const params = {
+        Protocol: protocol, /* required */
+        TopicArn: this.getTopicArn(topicName), /* required */
+        Endpoint: endpoint
+      }
+
+      if (filterPolicy)
+      params.Attributes = {
+        'FilterPolicy': filterPolicy,
+      }
+
+
+      this.sns.subscribe(params, (err, data) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+        return resolve(data);
+      })
+    });
+  }
+
+  getSubscriptions() {
+    return new Promise(function(resolve, reject) {
+      const params = {};
+
+      this.sns.listSubscriptions(params, (err, data) => {
+        if (err) {
+          console.log(err);
+          return reject(err);
+        }
+        async.map(data.Subscriptions, (s, cb) => {
+          const getSubAttParams = {
+            SubscriptionArn: s.SubscriptionArn
+          }
+          this.sns.getSubscriptionAttributes(getSubAttParams, (err, data) => {
+            if (err) {
+              console.log(err);
+              return cb(err);
+            }
+            var ret = Object.assign({},s);
+            ret.FilterPolicy = data.FilterPolicy;
+            cb(null,ret);
+          })
+        }, (err, results) => {
+          if (err) {
+            console.log(err);
+            return reject(err);
+          }
+          var topics = {};
+          const topicArnRegex = /(arn):(aws):(.*):(.*):(.*):(.*)/;
+          data.ForEach( (d) => {
+            const topicNameParts = topicArnRegex.exec(d.TopicArn);
+            const topicName = parsed[6];
+            if (!topics[topicName]) topics[topicName] = [];
+            topics[topicName].push(d);
+          })
+          return resolve(topics);
+        })
       });
     });
   }
